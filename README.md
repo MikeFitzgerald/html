8c7c8e1009d203ee7eb75d2038e512b5db632202
Mike Fitzgerald <mikeyfitzy@gmail.com>
2017-10-10 10:02:44 -0400

    Add(css-grid, flexbox, scss): Work in progress.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # **html**
 Some working examples of HTML layouts using various techniques.
 
+## **Setup**
+```
+npm install
+npm start
+```
+
+## **TODO**
+- [ ] Add /css-grid and /flexbox directories to webpack-dev-server.
+
+## **References**
+- [1] [tutorialzine - CSS Grid VS Flexbox: A Practical Comparison](https://tutorialzine.com/2017/03/css-grid-vs-flexbox)
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: './src/js/index.js',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     port: 3000
+  },
+  module: {
+    rules: [
+      { test: /\.html$/, use:'raw-loader' }
+    ]
   },
   output: {
     filename: 'bundle.js',
